db37cfc88396bfb57b84d318c0a1a3dfc1c1e19c
Juan Acevedo <jmamartinez18@gmail.com>
2023-11-17 21:19:49 -0700

    got current time to display in background to link current time to scedule besides date

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,14 +1,14 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var currentDate = $('#currentDay');
+var currentDate = $("#currentDay");
+var updateBlocks = $(".row")
+
 
 function displayTime() {
-  var rightNow = dayjs().format('dddd, MMMM, d');
+  var rightNow = dayjs().format('dddd, MMMM, D');
   currentDate.text(rightNow);
 }
-
-displayTime ();
 
 $(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -23,10 +23,33 @@ $(document).ready(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  function updateBlocks() {
+    var currentHour = dayjs().hour();
+    
+    $("[id^='hour-']").each(function() {
+      var elementID = $(this).attr("id");
+      var blockHour = parseInt (elementID.split ("-")[1]);
+      
+      $(this).removeClass("past present future");
+      
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future")
+      }
+    })
+
+  }
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  displayTime ();
+  
+  updateBlocks();
+  setInterval(updateBlocks, 60000);
 });
