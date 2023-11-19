2161b49dcf88a7b15dd3c1e9e1c1c1ef6ac2d774
Juan Acevedo <jmamartinez18@gmail.com>
2023-11-18 20:23:19 -0700

    final touches to code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,29 +4,37 @@ var updateBlocks = $(".row");
 var eventDescription = $(".description");
 var savedMessage = $(".messageSaved");
 
-
+//function calls time to be displayed in extended format
 function displayTime() {
   var rightNow = dayjs().format('dddd, MMMM, D');
   currentDate.text(rightNow);
 }
 
-$(document).ready(function () {
 
+//makes sure to load this first before rendering
+$(document).ready(function () {
+//this functions calls for the saving of local storage and the message to display when save button is pressed
   $(".saveBtn").on("click", function() {
     var descriptionId = $(this).closest('.row').attr('id'); 
     var userInput = $(this).prev('.description').val();
     localStorage.setItem(descriptionId, userInput);
     
+    //message displayed directing to created messageSaved Div/class in the HTMl file 
+    //once button is pressed
     var savedMessage = $(".messageSaved");
-    savedMessage.text("Event saved 🎉!").show();
+    savedMessage.text("Event saved! 🎉").show();
 
+    //this allows to load up to next message forcing a timeout of the first. Even though fade in and out is in css
+    //that is just for the effect not for the function itself. 
     setTimeout(function() {
       savedMessage.hide();
-    }, 2000);
-    
+    }, 3000);
+
   });
 
-
+  //this calls to update the blocks based off the hour as each hour has a different ID 2nd part. This applies it to 
+  //all while also allowing it to not target the entire ID. This removes the style applied and based on the current hour 
+  //displayed this will promt the correct color to show 
   function updateBlocks() {
     var currentHour = dayjs().hour();
     
@@ -46,6 +54,7 @@ $(document).ready(function () {
     })
   }
  
+  //this allows the browser to pull up the stored information based on the ID hour and keep that in place
   $('.description').each(function() {
     var descriptionId = $(this).closest('.row').attr('id'); 
     var savedDescription = localStorage.getItem(descriptionId);
@@ -54,8 +63,11 @@ $(document).ready(function () {
     }
   });
 
+  //calls to display time 
   displayTime ();
   
+  //this calls to update the hour blocks every minute
   updateBlocks();
   setInterval(updateBlocks, 60000);
+
 });
